18dc18810e59bbd6588037069be7735f8927a522
SylarK <syst.lucasamado@gmail.com>
2020-07-25 20:14:27 -0300

    Learning about generic views. Aprendendo sobre generic views e alguns conceitos em patterns

--- newboston/website/music/urls.py
+++ newboston/website/music/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     # music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # music/id
-    url(r'^(?P<album_id>[0-9])+/$', views.detail, name='detail'),
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+    url(r'^(?P<pk>[0-9])+/$', views.DetailView.as_view(), name='detail')
 ]   
