276686c0a472849177820bc805d390230c9ea7d8
SylarK <syst.lucasamado@gmail.com>
2020-07-26 10:14:57 -0300

    Update view and Delete View.. using classes

--- newboston/website/music/urls.py
+++ newboston/website/music/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     # music/
     url(r'^$', views.IndexView.as_view(), name='index'),
     # music/id
-    url(r'^(?P<pk,>[0-9])+/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9])+/$', views.DetailView.as_view(), name='detail'),
     #music/album/add
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    #music/album/2
+    url(r'album/add/(?P<pk>[0-9])+/$', views.AlbumUpdate.as_view(), name='album-update'),
+    #music/album/2/delete
+    url(r'album/add/(?P<pk>[0-9])+/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]   
