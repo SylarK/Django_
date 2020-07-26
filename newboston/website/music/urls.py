9bdfa4146312dae36145ce0d0a62fffe5d15c127
SylarK <syst.lucasamado@gmail.com>
2020-07-26 09:29:05 -0300

    Intro Models Form with Django

--- newboston/website/music/urls.py
+++ newboston/website/music/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # music/
     url(r'^$', views.IndexView.as_view(), name='index'),
     # music/id
-    url(r'^(?P<pk>[0-9])+/$', views.DetailView.as_view(), name='detail')
+    url(r'^(?P<pk,>[0-9])+/$', views.DetailView.as_view(), name='detail'),
+    #music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
 ]   
