3a7f4401021dc0f55d056397949900e9405b7add
unknown <lucasamadort@gmail.com>
2020-08-02 19:26:03 -0300

    Editar e gerenciar estão funcionando

--- _forumSimulation/forum/interface/urls.py
+++ _forumSimulation/forum/interface/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('welcome/', views.welcome, name='welcome'),
     path('new/', views.newpost, name='newpost'),
-    path('my/<int:user_id>', views.mypost, name='mypost')
+    path('my/<int:user_id>', views.mypost, name='mypost'),
+    path('edit/<int:item_id>', views.edit, name='edit'),
+    path('erase/<int:item_id>', views.erase, name='erase'),
+    path('post', views.post, name='post')
 ]
--- _forumSimulation/forum/interface/views.py
+++ _forumSimulation/forum/interface/views.py
@@ -34,3 +34,22 @@ def newpost(request):
         npost.save()
 
         return redirect('interface:welcome')
+
+def edit(request, item_id):
+    item = Posts.objects.get(id=item_id)
+    if request.method == 'GET':
+        return render(request, 'interface/edit.html', {'item':item})
+    else:
+        item.title = request.POST['title']
+        item.text = request.POST['text']
+
+        item.save()
+
+        return redirect('interface:welcome')
+
+def erase(request, item_id):
+    Posts.objects.filter(id=item_id).delete()
+    return redirect('interface:welcome')
+
+def post(request):
+    return render(request, 'interface/view.html')    
